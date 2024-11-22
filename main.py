0afa7aea9b213335de7e55b508245c093ff61ef9
Adam <adamos139@wp.pl>
2024-11-22 14:31:56 +0100

    Facade implemented

--- main.py
+++ main.py
@@ -13,7 +13,41 @@ class Book:
         self.year = year
 
     def __str__(self) -> str:
-        return f"{self.name}, {self.year}, id: {self.identify}"
+        return f"{self.name}, {self.year}, id {self.identify}"
+
+    def __repr__(self) -> str:
+        return f"{self.name}, {self.year}, id {self.identify}"
+
+
+#Classes for Factory
+class User:
+    def __init__(self, name: str):
+        self.name = name
+        self.limit = 0
+        self.books = []
+        raise NotImplementedError("User is supposed to be an abstract class")
+
+
+class Student(User):
+    def __init__(self, name: str):
+        self.name = name
+        self.limit = 5
+        self.books = []
+
+
+class Teacher(User):
+    def __init__(self, name: str):
+        self.name = name
+        self.limit = 15
+        self.books = []
+
+
+class Librarian(User):
+    def __init__(self, name: str):
+        self.name = name
+        self.limit = 25
+        self.books = []
+
 
 #Singleton + Iterator
 class LibraryCatalog(object):
@@ -53,65 +87,61 @@ class LibraryCatalog(object):
             self.current = 0
         self.current += 1
         book_key = list(self.get_catalog().keys())[self.current-1] 
-        return f"{book_key}, {self.catalog[book_key][0]}/{self.catalog[book_key][1]}"
+        return f"{book_key}, count: {self.catalog[book_key][0]}/{self.catalog[book_key][1]}"
         
     def add_book(self, book: Book) -> int:
-        if book not in self.catalog:
+        for i in list(self.catalog.keys()):
+            if (i.name, i.identify, i.year) == (book.name, book.identify, book.year):
+                self.catalog[i][0] += 1
+                self.catalog[i][1] += 1
+                return 1
+        else:
             self.catalog[book] = [1, 1]
             return 2
-        else:
-            self.catalog[book][0] += 1
-            self.catalog[book][1] += 1
-        return 1
+        
+    def borrow_book(self, user: User, identify: int) -> int:
+        if user.limit <= len(user.books):
+            #Max book limit reached
+            return -1
+        for i in self.catalog:
+            if i.identify == identify:
+                for j in user.books:
+                    if j[0].identify == identify:
+                        #User already has this book
+                        return -2
+                if self.catalog[i][0] < 1:
+                    #Book unavailable right now, add user to observers
+                    #Observer code
+                    return 0
+                else:
+                    #Give book to user's list as ordered (not taken yet)
+                    user.books.append([i, "Ordered"])
+                    self.catalog[i][0] -= 1
+                    return 1
+        #This book is not in catalog
+        return -3
+            
+    def return_book(self, user:User, identify: int) -> int:
+        if len(user.books) < 1:
+            #What does user want to return?
+            return -1
+        for i in self.catalog:
+            if i.identify == identify:
+                for j in user.books:
+                    if j[0].identify == identify:
+                        #Everything is in order
+                        user.books.remove(j)
+                        self.catalog[i][0] += 1
+                        return 1
+                #User did not borrow this book
+                return -2
+        #This book is not in catalog
+        return -3
 
 #Adapter
 class DataAdapter:
     def read():
         pass
-
-
-#Classes for Factory
-class User:
-    def __init__(self, name: str):
-        self.name = name
-        self.limit = 0
-        self.books = []
-        raise NotImplementedError("User is supposed to be an abstract class")
-
-    def borrow_book(identify: int) -> bool:
-        if self.limit >= len(self.books):
-            #log "max book limit reached"
-            return False
-        #Code for facade - find book, borrow it or not, connect user with Observer
-        return True
-            
-    def return_book(identify: int) -> bool:
-        for i in self.books:
-            if i.identify == identify:
-                #Code for Facade, make book available, update Observer
-                return True
-        return False
-
-
-class Student(User):
-    def __init__(self, name: str):
-        self.name = name
-        self.limit = 5
-        self.books = []
-
-
-class Teacher(User):
-    def __init__(self, name: str):
-        self.name = name
-        self.limit = 15
-        self.books = []
-
-
-class Librarian(User):
-    def __init__(self, name: str):
-        self.name = name
-        self.limit = 25
-        self.books = []
 
 
 #Factory
@@ -132,44 +162,52 @@ class ActionInterface:
     def __init__(self, catalog: LibraryCatalog):
         self.catalog = catalog
         
-    def add_book(self, book):
+    def add_book(self, book) -> bool:
         return self.catalog.add_book(book)
         
-    def show_catalog(self):
+    def show_catalog(self) -> dict:
         return self.catalog.get_catalog()
     
-    def show_any_book(self):
+    def show_any_book(self) -> str:
         return self.catalog.get_next()
         
-    def borrow_book(self):
-        pass
+    def borrow_book(self, user: User, identify: int) -> int:
+        return self.catalog.borrow_book(user, identify)
     
-    def return_book(self):
-        pass
+    def return_book(self, user: User, identify: int) -> int:
+        return self.catalog.return_book(user, identify)
 
 #Observer
 
 
 a = LibraryCatalog()
-for i in ["A", "B", "C", "D"]:
+for i in [Book("A", 0, 1999), Book("B", 1, 2002), Book("C", 2, 2004), Book("D", 3, 2020)]:
     a.add_book(i)
-print(a.get_catalog())
+print(f"Called LibraryCatalog.get_catalog(): {a.get_catalog()}")
 b = LibraryCatalog()
-print(a is b)
-print(a == b)
-print(range(5) is range(5))
-print(range(5) == range(5))
+print(f"Called 'a is b': {a is b}")
+print(f"Called 'a == b': {a == b}")
+print(f"Called 'range(5) is range(5)': {range(5) is range(5)}")
+print(f"Called 'range(5) == range(5)': {range(5) == range(5)}")
+print(f"Called Iterator 7 times:")
 for i in range(7):
     print(a.get_next())
+print(f"Creating abstract class User ...")
 try:
     error_class = User("A")
 except Exception as e:
     print(e)
+print(f"Creating unknown user type ...")
 try:
-    error_class = UserFactory.create_user(User, "A")
+    error_class = UserFactory.create_user("user", "A")
 except Exception as e:
     print(e)
 test_interface = ActionInterface(a)
-print(test_interface.add_book("D"))
-print(test_interface.show_catalog())
-print(test_interface.show_any_book())
+print(f"Called Facade add_book(): {test_interface.add_book(Book('D', 3, 2020))}")
+print(f"Called Facade show_catalog(): {test_interface.show_catalog()}")
+print(f"Called Facade show_any_book(): {test_interface.show_any_book()}")
+
+test_user = UserFactory.create_user("student", "XYZ")
+print(f"Called Facade borrow_book: {test_interface.borrow_book(test_user, 3)}")
+print(f"Called Facade borrow_book again: {test_interface.borrow_book(test_user, 3)}")
+print(f"Called Facade return_book: {test_interface.return_book(test_user, 3)}")
