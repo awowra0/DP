25432d640667e41c462cdf0b0ed3f1fa0dc283fb
Adam <adamos139@wp.pl>
2024-11-23 17:21:38 +0100

    pycodestyle

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 class Book:
     """
     Class representing book.
-    
+
     Parameters:
     name (str): Book name.
     identify (int): Book ID.
@@ -19,14 +19,14 @@ class Book:
         return f"{self.name}, {self.year}, id {self.identify}"
 
 
-#Classes for Factory
+# Classes for Factory
 class User:
     """
     Abstract class representing user. Do not use it directly.
-    
+
     Constructor parameters:
     name (str): User name.
-    
+
     Parameters:
     name (str): User name.
     limit (int): Book limit.
@@ -42,10 +42,10 @@ class User:
 class Student(User):
     """
     Class representing student.
-    
+
     Constructor parameters:
     name (str): User name.
-    
+
     Parameters:
     name (str): User name.
     limit (int): Book limit.
@@ -60,10 +60,10 @@ class Student(User):
 class Teacher(User):
     """
     Class representing teacher.
-    
+
     Constructor parameters:
     name (str): User name.
-    
+
     Parameters:
     name (str): User name.
     limit (int): Book limit.
@@ -78,10 +78,10 @@ class Teacher(User):
 class Librarian(User):
     """
     Class representing librarian.
-    
+
     Constructor parameters:
     name (str): User name.
-    
+
     Parameters:
     name (str): User name.
     limit (int): Book limit.
@@ -93,50 +93,51 @@ class Librarian(User):
         self.books = []
 
 
-#Observer
+# Observer
 class Observer:
     """
     Class representing Observer design pattern.
-    
+
     Constructor parameters:
     user (User): Reference to user who awaits book.
     book (Book): Observed book.
-    
+
     Parameters:
     user (User): Reference to user who awaits book.
     books (list(Book)): List of observed books.
     infos (list(str)): A list of notification sent to user.
-    
+
     Methods:
-    update(text: str) -> str: Adds new notification. 
+    update(text: str) -> str: Adds new notification.
     """
     def __init__(self, user: User, book: Book):
         self.user = user
         self.books = [book]
         self.infos = []
-    
+
     def update(self, text: str) -> str:
         self.infos.append(text)
         return text
 
+
 class ObserverManager:
     """
     Class representing manager for Observer design pattern.
-    
+
     Parameters:
     observers (list(Observer)): List of observers.
-    
+
     Methods:
     attach(user: User, book: Book) -> int: Tries to create a new observer from given user and book. Adds new observer and returns 1 if user is not an observer, adds new book to observer's list and returns 0 if user is one or returns -1 if user has already been waiting for this book.
     deattach(user: User, book: Book) -> int: Tries to remove book from user's observer instance. Removes this book and returns 1 if the book is in observer's list, return 0 if observer has no book in list, returns -1 if book is not in observer's list or returns -2 if given user is not an observer.
     notify(book: Book): Notifies proper observers that their book is currently available.
-    
+
     Notes:
     Why Observer? It gives an opportunity to create an automated system that sends information about books to interested users as soon as possible.
     """
     def __init__(self):
         self.observers = []
-    
+
     def attach(self, user: User, book: Book) -> int:
         for i in self.observers:
             if i.user.name == user.name:
@@ -150,55 +151,54 @@ class ObserverManager:
         self.observers.append(Observer(user, book))
         self.observers[-1].update(f"User {self.observers[-1].user.name} wishlisted book {book}.")
         return 1
-    
+
     def deattach(self, user: User, book: Book) -> 1:
         for i in self.observers:
             if i.user.name == user.name:
                 if len(user.books) < 1:
-                    #User has no book in wishlist
+                    # User has no book in wishlist
                     return 0
                 for j in i.books:
                     if j.identify == book.identify:
-                        #remove book from wishlist
+                        # Remove book from wishlist
                         i.books.remove(j)
                         return 1
-                #Book not found in wishlist
+                # Book not found in wishlist
                 return -1
-        #Observer not found
+        # Observer not found
         return -2
-    
+
     def notify(self, book):
         for i in self.observers:
             for j in i.books:
                 if j.identify == book.identify:
-                    #Inform observer
+                    # Inform observer
                     i.update(f"User {i.user.name} - book {book} is available.")
                     break
 
 
-#Singleton + Iterator
+# Singleton + Iterator
 class LibraryCatalog(object):
     """
     Class representing book catalog in library.
-    It uses Singleton pattern design (one catalog is enough) and 
-    Iterator (to show next books in catalog).
-    
+    It uses Singleton pattern design (one catalog is enough) and Iterator (to show next books in catalog).
+
     Constructor parameters:
     None
-    
+
     Parameters:
     instance (LibraryCatalog): Singleton instance.
     catalog (dict(Book: list(int, int))): Dictionary with books in catalog, their available count and total count.
     current (int): Index used for Iterator design pattern in get_next() method.
-    
+
     Methods:
     get_catalog() -> dict: Returns full catalog.
     get_next() -> str: Returns next book in catalog.
     add_book(book: Book) -> int: Adds book to catalog. Increases count and returns 1 if book already exists, otherwise returns 2.
     borrow_book(user: User, identify: int, manager: ObserverManager) -> int: Tries to find a book by its ID and declare one of copies as ordered by user. Decreases book count and returns 1 if user has not reached book limit and book is available, 0 if book is unavailable right now, -1 if user reached book limit, -2 if user already ordered this book or -3 if book does not exist. Informs ObserverManager to notify users.
     return_book(user: User, identify: int, manager: ObserverManager) -> int: Tries to find a book by its ID and return it to library. Increases book count, removes it from user's list and returns 1 if the book exists and user has it, -1 if user has no book borrowed, -2 if user has not borrowed this book or -3 if the book does not exist. Informs ObserverManager to notify users.
-    update_borrow(user: User, identify: int) -> int: Tries to update book's status in user's list from "Ordered" to "Borrowed". Changes status and returns 1 if user has this book and it is "Ordered", -1 if the book is not "Ordered" or -2 if user does not have this book in its list. 
-    
+    update_borrow(user: User, identify: int) -> int: Tries to update book's status in user's list from "Ordered" to "Borrowed". Changes status and returns 1 if user has this book and it is "Ordered", -1 if the book is not "Ordered" or -2 if user does not have this book in its list.
+
     Notes:
     Why Singleton? Library needs only one catalog for books. Creating second one may make a mess with searching in two catalog and this would be troublesome.
     Why Iterator? Catalog may become very big in time and looking at dictionary of all books can be confusing. By showing one book lineally, user can look at different book every time if catalog is big enough.
@@ -209,20 +209,20 @@ class LibraryCatalog(object):
             self.catalog = {}
             self.current = 0
         return self.instance
-    
+
     def get_catalog(self) -> list:
         return self.catalog
-        
-    #Iterator
+
+    # Iterator
     def get_next(self) -> Book:
         if len(self.get_catalog()) < 1:
             return None
         if self.current == len(self.get_catalog()):
             self.current = 0
         self.current += 1
-        book_key = list(self.get_catalog().keys())[self.current-1] 
+        book_key = list(self.get_catalog().keys())[self.current-1]
         return f"{book_key}, count: {self.catalog[book_key][0]}/{self.catalog[book_key][1]}"
-        
+
     def add_book(self, book: Book) -> int:
         for i in list(self.catalog.keys()):
             if (i.name, i.identify, i.year) == (book.name, book.identify, book.year):
@@ -232,76 +232,77 @@ class LibraryCatalog(object):
         else:
             self.catalog[book] = [1, 1]
             return 2
-        
+
     def borrow_book(self, user: User, identify: int, manager: ObserverManager) -> int:
         if user.limit <= len(user.books):
-            #Max book limit reached
+            # Max book limit reached
             return -1
         for i in self.catalog:
             if i.identify == identify:
                 for j in user.books:
                     if j[0].identify == identify:
-                        #User already has this book
+                        # User already has this book
                         return -2
                 if self.catalog[i][0] < 1:
-                    #Book unavailable right now, add user to observers
+                    # Book unavailable right now, add user to observers
                     manager.attach(user, i)
                     return 0
                 else:
-                    #Give book to user's list as ordered (not taken yet)
+                    # Give book to user's list as ordered (not taken yet)
                     user.books.append([i, "Ordered"])
                     self.catalog[i][0] -= 1
                     manager.deattach(user, i)
                     return 1
-        #This book is not in catalog
+        # This book is not in catalog
         return -3
-            
-    def return_book(self, user:User, identify: int, manager: ObserverManager) -> int:
+
+    def return_book(self, user: User, identify: int, manager: ObserverManager) -> int:
         if len(user.books) < 1:
-            #What does user want to return?
+            # What does user want to return?
             return -1
         for i in self.catalog:
             if i.identify == identify:
                 for j in user.books:
                     if j[0].identify == identify:
-                        #Everything is in order
+                        # Everything is in order
                         user.books.remove(j)
                         self.catalog[i][0] += 1
                         if self.catalog[i][0] == 1:
-                            #Book is available, inform observers
+                            # Book is available, inform observers
                             manager.notify(i)
                         return 1
-                #User did not borrow this book
+                # User did not borrow this book
                 return -2
-        #This book is not in catalog
+        # This book is not in catalog
         return -3
-        
+
     def update_borrow(self, user: User, identify: int) -> int:
         for i in user.books:
             if i[0].identify == identify:
                 if i[1] == "Ordered":
-                    #User took the book
+                    # User took the book
                     i[1] = "Borrowed"
                     return 1
-                #Book is not ordered (it is probably taken already)
+                # Book is not ordered (it is probably taken already)
                 return -1
-        #User does not have this book in list
+        # User does not have this book in list
         return -2
 
-#Adapter
+
+# Adapter
 class DataAdapter:
     def read():
         pass
 
 
-#Factory
+# Factory
 class UserFactory:
     """
     Class implementing Factory design pattern to create new users.
-    
+
     Methods:
     create_user(user: str, name: str) -> User: Tries to create a proper user class. Return Student, Teacher or Librarian class with chosen name or raises Error for different class names.
-    
+
     Notes:
     Why Factory? It is a simple solution to create multiple various accounts. Now it is obvious what classes are available plus method input can be universal.
     """
@@ -316,19 +317,19 @@ class UserFactory:
             raise TypeError("Unknown user kind inserted.")
 
 
-#Facade
+# Facade
 class ActionInterface:
     """
     Class implementing Facade design pattern to show available commands as simple 'interface'.
-    
+
     Constructor parameters:
     catalog (LibraryCatalog): Catalog that does every method presented by interface.
     manager (ObserverManager): Manager for Observers awaiting for books.
-    
+
     Parameters:
     catalog (LibraryCatalog): Catalog that does every method presented by interface.
     manager (ObserverManager): Manager for Observers awaiting for books.
-    
+
     Methods:
     get_catalog() -> dict: Returns full catalog.
     get_next() -> str: Returns next book in catalog.
@@ -336,25 +337,25 @@ class ActionInterface:
     borrow_book(user: User, identify: int, manager: ObserverManager) -> int: Tries to find a book by its ID and declare one of copies as ordered by user. Decreases book count and returns 1 if user has not reached book limit and book is available, 0 if book is unavailable right now, -1 if user reached book limit, -2 if user already ordered this book or -3 if book does not exist. Informs ObserverManager to notify users.
     return_book(user: User, identify: int, manager: ObserverManager) -> int: Tries to find a book by its ID and return it to library. Increases book count, removes it from user's list and returns 1 if the book exists and user has it, -1 if user has no book borrowed, -2 if user has not borrowed this book or -3 if the book does not exist. Informs ObserverManager to notify users.
     update_borrow(user: User, identify: int) -> int: Tries to update book's status in user's list from "Ordered" to "Borrowed". Changes status and returns 1 if user has this book and it is "Ordered", -1 if the book is not "Ordered" or -2 if user does not have this book in its list.
-    
+
     Why Facade? It allows to show available commands that are located in more advanced and complicated class. Here everything looks better and complex processing is hidden from view.
     """
     def __init__(self, catalog: LibraryCatalog, manager: ObserverManager):
         self.catalog = catalog
         self.manager = manager
-        
+
     def add_book(self, book: Book) -> bool:
         return self.catalog.add_book(book)
-        
+
     def show_catalog(self) -> dict:
         return self.catalog.get_catalog()
-    
+
     def show_any_book(self) -> str:
         return self.catalog.get_next()
-        
+
     def borrow_book(self, user: User, identify: int) -> int:
         return self.catalog.borrow_book(user, identify, self.manager)
-    
+
     def return_book(self, user: User, identify: int) -> int:
         return self.catalog.return_book(user, identify, self.manager)
 
--- main_test.py
+++ main_test.py
@@ -7,7 +7,7 @@ class Tester():
         self.users = []
         self.manager = ObserverManager()
         self.interface = ActionInterface(self.cat, self.manager)
-        
+
     def start_tests(self):
         self.test_1_add_book()
         self.test_2_get_catalog()
@@ -27,106 +27,106 @@ class Tester():
         self.test_16_observers_notify()
         self.test_17_observers_deattach()
         print("Tests finished.")
-    
+
     def test_1_add_book(self):
         expect = 2
         for i in [Book("A", 0, 1999), Book("B", 1, 2002), Book("C", 2, 2004), Book("D", 3, 2020)]:
             got = self.cat.add_book(i)
-            assert (expect==got)
+            assert (expect == got)
         print(f"Test 1 - added 4 books.")
-    
+
     def test_2_get_catalog(self):
         expect = self.cat.catalog
         got = self.cat.get_catalog()
-        assert (expect==got)
+        assert (expect == got)
         print(f"Test 2 - catalog shown")
-        
+
     def test_3_singleton(self):
         temp = LibraryCatalog()
         expect = [True, True]
         got = [self.cat is temp, self.cat == temp]
-        assert (expect==got)
+        assert (expect == got)
         print(f"Test 3 - singleton")
-        
+
     def test_4_iterator(self):
         expect = "C, 2004, id 2, count: 1/1"
         for i in range(7):
             got = self.cat.get_next()
-        assert (expect==got)
+        assert (expect == got)
         print(f"Test 4 - iterator")
-        
+
     def test_5_factory_success(self):
         expect = 3
         self.users.append(UserFactory.create_user("student", "AAA"))
         self.users.append(UserFactory.create_user("teacher", "BBB"))
         self.users.append(UserFactory.create_user("librarian", "CCC"))
         got = len(self.users)
-        assert (expect==got)
+        assert (expect == got)
         print(f"Test 5 - factory created users")
-    
+
     def test_6_factory_fail(self):
         expect = "Unknown user kind inserted."
         try:
             got = UserFactory.create_user("user", "A")
         except Exception as e:
             got = str(e)
-        assert (expect==got)
+        assert (expect == got)
         print(f"Test 6 - factory refused to create unknown user")
-    
+
     def test_7_abstract_class(self):
         expect = "User is supposed to be an abstract class"
         try:
             got = User("A")
         except Exception as e:
             got = str(e)
-        assert (expect==got)
+        assert (expect == got)
         print(f"Test 7 - abstract class")
-        
+
     def test_8_facade_add_copy_book(self):
         expect = 1
         got = self.interface.add_book(Book('D', 3, 2020))
-        assert (expect==got)
+        assert (expect == got)
         print(f"Test 8 - facade book copy")
-        
+
     def test_9_facade_show_catalog(self):
         expect = self.cat.catalog
         got = self.interface.show_catalog()
-        assert (expect==got)
+        assert (expect == got)
         print(f"Test 9 - facade catalog shown")
-        
+
     def test_10_facade_iterator(self):
         expect = "D, 2020, id 3, count: 2/2"
         got = self.interface.show_any_book()
-        assert (expect==got)
+        assert (expect == got)
         print(f"Test 10 - facade book shown")
-        
+
     def test_11_facade_borrow_book(self):
         expect = 1
         got = self.interface.borrow_book(self.users[0], 3)
-        assert (expect==got)
+        assert (expect == got)
         print(f"Test 11 - facade book borrowed")
-        
+
     def test_12_facade_borrow_book_fail(self):
         expect = -2
         got = self.interface.borrow_book(self.users[0], 3)
-        assert (expect==got)
+        assert (expect == got)
         print(f"Test 12 - facade refused to borrow same book")
-        
+
     def test_13_facade_update_book(self):
         expect = ["Ordered", 1, "Borrowed"]
         got = []
         got.append(self.users[0].books[0][1])
         got.append(self.interface.update_borrow(self.users[0], 3))
         got.append(self.users[0].books[0][1])
-        assert (expect==got)
+        assert (expect == got)
         print(f"Test 13 - facade updated book status")
-        
+
     def test_14_observers_empty(self):
         expect = []
         got = self.interface.manager.observers
-        assert (expect==got)
+        assert (expect == got)
         print(f"Test 14 - no observers yet")
-        
+
     def test_15_observers_attach(self):
         expect = [0, 1, ['User BBB wishlisted book B, 2002, id 1.']]
         got = []
@@ -134,23 +134,23 @@ class Tester():
         got.append(self.interface.borrow_book(self.users[1], 1))
         got.append(len(self.interface.manager.observers))
         got.append(self.interface.manager.observers[-1].infos)
-        assert (expect==got)
+        assert (expect == got)
         print(f"Test 15 - observer attached")
-        
+
     def test_16_observers_notify(self):
         expect = "User BBB - book B, 2002, id 1 is available."
         self.interface.return_book(self.users[0], 1)
         got = self.interface.manager.observers[-1].infos[-1]
-        assert (expect==got)
+        assert (expect == got)
         print(f"Test 16 - observer notified")
-        
+
     def test_17_observers_deattach(self):
         expect = []
         self.interface.borrow_book(self.users[1], 1)
         got = self.interface.manager.observers[-1].books
-        assert (expect==got)
+        assert (expect == got)
         print(f"Test 17 - observer deattached - borrowed book")
-        
+
 
 if __name__ == "__main__":
     a = Tester()
